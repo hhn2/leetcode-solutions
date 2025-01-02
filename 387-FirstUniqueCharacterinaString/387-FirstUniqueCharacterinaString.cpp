a4668980b5f3780325dce981b5fee4d113626a8a
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2025-01-02 14:14:58 +0100

    Update file 1/2/2025, 2:14:58 PM

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.cpp
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.cpp
@@ -1,42 +1,18 @@
-class Solution {
+#include<string>
+#include<vector>
+using namespace std;
+class Solution{
 public:
-    // Function to check if a character exists in the map
-    int contains(std::map<int, char>& myMap, char myChar) {
-        for (auto it = myMap.begin(); it != myMap.end(); it++) {
-            if (it->second == myChar) {
-                return it->first; // Return the key (index)
-            }
-        }
-        return -1; // Return -1 if not found
-    }
-
-    int firstUniqChar(std::string s) {
-        std::map<int, char> myMap;
-        std::set<char> repeated; // Track characters that have been repeated
-
-        // Populate the map with unique characters
-        for (int i = 0; i < s.length(); i++) {
-            // Skip if the character is already marked as repeated
-            if (repeated.find(s[i]) != repeated.end()) {
-                continue;
-            }
-
-            int existingIndex = contains(myMap, s[i]);
-            if (existingIndex == -1) {
-                // Add character to the map with its index as the key
-                myMap[i] = s[i];
-            } else {
-                // Remove character from the map if it's repeated
-                myMap.erase(existingIndex);
-                repeated.insert(s[i]); // Mark as repeated
-            }
-        }
-
-        // Find the first unique character by iterating over the map
-        if (!myMap.empty()) {
-            return myMap.begin()->first; // The first key in the map
-        }
-
-        return -1; // No unique character found
-    }
+int firstUniqChar(string s){
+vector<int>freq(26,0);
+for(char c:s){
+freq[c-'a']++;
+}
+for(int i=0;i<s.size();i++){
+if(freq[s[i]-'a']==1){
+return i;
+}
+}
+return -1;
+}
 };
