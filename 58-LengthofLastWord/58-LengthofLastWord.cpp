ba4cf5e19b178c3b2f831a9417cac2632aacdca6
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2025-01-08 12:50:07 -0500

    Create file

---
+++ 58-LengthofLastWord/58-LengthofLastWord.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        std::istringstream iss(s); 
+        std::string word;
+
+        while (iss >>word){
+            continue;
+        }
+
+        return word.length();  
+
+    }};
+        
