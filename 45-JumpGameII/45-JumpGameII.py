dc5bddfd63080f08530fc29d6112a6563371f8a3
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2025-03-22 17:40:45 -0400

    File updated at 3/22/2025, 5:40:45 PM

--- 45-JumpGameII/45-JumpGameII.py
+++ 45-JumpGameII/45-JumpGameII.py
@@ -1,27 +1,16 @@
-// Last updated: 3/22/2025, 5:40:06 PM
+// Last updated: 3/22/2025, 5:40:45 PM
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n = len(nums)
-        memo = {}
-
-        def backtrack(i):
-            if i >= n - 1:
-                return 0  # No jumps needed if at or beyond the end
-            
-            if i in memo:
-                return memo[i]
-            
-            min_jumps = float('inf')
-            max_jump = nums[i]
-            max_reachable_index = min(i + max_jump, n - 1)
-            
-            # Try all possible jumps
-            for new_index in range(i + 1, max_reachable_index + 1):
-                jumps_needed = backtrack(new_index)
-                if jumps_needed != float('inf'):
-                    min_jumps = min(min_jumps, jumps_needed + 1)
-            
-            memo[i] = min_jumps
-            return min_jumps
+        jumps = 0
+        current_end = 0
+        farthest = 0
         
-        return backtrack(0)
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+            
+            # Time to use a jump
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+
+        return jumps
