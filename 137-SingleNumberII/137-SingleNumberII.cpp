a3ba2740d437283bb86ae0ffb32444cbeb3d65a7
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2025-04-19 17:26:37 -0400

    Create file

---
+++ 137-SingleNumberII/137-SingleNumberII.cpp
@@ -0,0 +1,24 @@
+// Last updated: 4/19/2025, 5:26:36 PM
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        vector<int> bits(32, 0);  // Use 32 bits to include sign bit
+        
+        // Count bits at each position
+        for (int num : nums) {
+            // Process all 32 bits including sign bit
+            for (int i = 0; i < 32; i++) {
+                bits[i] += (num >> i) & 1;
+                bits[i] %= 3;
+            }
+        }
+        
+        // Reconstruct the number
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result |= (bits[i] << i);
+        }
+        
+        return result;
+    }
+};
