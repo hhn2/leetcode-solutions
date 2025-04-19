2c7a5b799c1bbb146787a1de9386b375927b81eb
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2025-04-19 17:35:51 -0400

    File updated at 4/19/2025, 5:35:51 PM

--- 137-SingleNumberII/137-SingleNumberII.py
+++ 137-SingleNumberII/137-SingleNumberII.py
@@ -1,9 +1,20 @@
-# Last updated: 4/19/2025, 5:33:13 PM
+# Last updated: 4/19/2025, 5:35:51 PM
 class Solution:
     def singleNumber(self, nums):
-        ones = 0
-        twos = 0
-        for num in nums:
-            ones = (ones ^ num) & ~twos
-            twos = (twos ^ num) & ~ones
-        return ones
+        ans = 0
+
+        for i in range(32):
+            bit_sum = 0
+            for num in nums:
+                # Convert the number to two's complement representation to handle large test case
+                if num < 0:
+                    num = num & (2**32-1)
+                bit_sum += (num >> i) & 1
+            bit_sum %= 3
+            ans |= bit_sum << i
+
+        # Convert the result back to two's complement representation if it's negative to handle large test case
+        if ans >= 2**31:
+            ans -= 2**32
+
+        return ans
