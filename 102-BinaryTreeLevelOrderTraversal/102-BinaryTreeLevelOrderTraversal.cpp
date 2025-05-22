abe65902c244d2f3fb2856551d632a1359973141
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2025-05-22 17:31:32 -0400

    Create file

---
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,50 @@
+// Last updated: 5/22/2025, 5:31:32 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        // bool is true when even, and false when odd
+        vector<vector<int>> v;
+        if (!root){
+            return v;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        
+
+        while (!q.empty()){
+        int size = q.size();
+
+        vector<int> sv;
+
+        for (int i = 0; i < size; i++){
+            auto hi = q.front();
+            sv.push_back(hi->val);
+            if(hi->left){
+                q.push(hi->left);
+            }
+            if(hi->right){
+                q.push(hi->right);
+            }
+            q.pop();
+            }
+             vector<int> svv = sv;
+        v.push_back(svv);
+        sv.clear();
+
+        }
+       return v;
+    }
+
+    
+};
