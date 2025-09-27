ba320e024fcdb1569c4861d8c905447b6fc74029
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2025-09-27 19:37:23 -0400

    File updated at 9/27/2025, 7:37:22 PM

--- 149-MaxPointsonaLine/149-MaxPointsonaLine.cpp
+++ 149-MaxPointsonaLine/149-MaxPointsonaLine.cpp
@@ -1,4 +1,4 @@
-// Last updated: 9/27/2025, 7:32:45 PM
+// Last updated: 9/27/2025, 7:37:22 PM
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
@@ -8,17 +8,11 @@ public:
 
         for (int i = 0; i < n; i++) {
             map<pair<int,int>, int> slopeCount;
-            int duplicates = 1;  // count the base point itself
             int localMax = 0;
 
             for (int j = i+1; j < n; j++) {
                 int dx = points[j][0] - points[i][0];
                 int dy = points[j][1] - points[i][1];
-
-                if (dx == 0 && dy == 0) {
-                    duplicates++;  // same point
-                    continue;
-                }
 
                 int g = gcd(dx, dy);
                 dx /= g;
@@ -28,7 +22,7 @@ public:
                 localMax = max(localMax, slopeCount[{dy, dx}]);
             }
 
-            result = max(result, localMax + duplicates);
+            result = max(result, localMax + 1); // +1 for the base point itself
         }
 
         return result;
