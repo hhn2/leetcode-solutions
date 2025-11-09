933cfa38dc5ff7fa13b97c67e261fc4ab1bcdaea
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2025-11-08 23:29:25 -0500

    File updated at 11/8/2025, 11:29:24 PM

--- 53-MaximumSubarray/53-MaximumSubarray.cpp
+++ 53-MaximumSubarray/53-MaximumSubarray.cpp
@@ -1,36 +1,48 @@
-// Last updated: 6/15/2025, 9:10:48 PM
+// Last updated: 11/8/2025, 11:29:24 PM
 class Solution {
 public:
-
-   int fixedi(vector<int>& nums, int l, int r) {
-    int maxx = INT_MIN, curr = 0;
-    for (int i = l; i <= r; ++i) {
-        curr += nums[i];
-        maxx = max(maxx, curr);
+    int maxSubArray(vector<int>& nums) {
+        return maxSubArrayHelper(nums, 0, nums.size() - 1);
     }
-    return maxx;
-}
-
-int fixedj(vector<int>& nums, int l, int r) {
-    int maxx = INT_MIN, curr = 0;
-    for (int i = r; i >= l; --i) {
-        curr += nums[i];
-        maxx = max(maxx, curr);
+    
+private:
+    int maxSubArrayHelper(vector<int>& nums, int left, int right) {
+        // Base case: only one element
+        if (left == right) {
+            return nums[left];
+        }
+        
+        int mid = left + (right - left) / 2;
+        
+        // Recursively find max in left and right halves
+        int leftMax = maxSubArrayHelper(nums, left, mid);
+        int rightMax = maxSubArrayHelper(nums, mid + 1, right);
+        
+        // Find max subarray crossing the middle
+        int crossMax = maxCrossingSum(nums, left, mid, right);
+        
+        // Return the maximum of the three
+        return max({leftMax, rightMax, crossMax});
     }
-    return maxx;
-}
-
-int helper(vector<int>& nums, int l, int r) {
-    if (l == r) return nums[l];
-    int mid = (l + r) / 2;
-    int s1 = helper(nums, l, mid);
-    int s2 = helper(nums, mid + 1, r);
-    int s3 = fixedj(nums, l, mid) + fixedi(nums, mid + 1, r);
-    return max({s1, s2, s3});
-}
-
-int maxSubArray(vector<int>& nums) {
-    return helper(nums, 0, nums.size() - 1);
-}
-
+    
+    int maxCrossingSum(vector<int>& nums, int left, int mid, int right) {
+        // Find max sum starting from mid and going left
+        int leftSum = INT_MIN;
+        int sum = 0;
+        for (int i = mid; i >= left; i--) {
+            sum += nums[i];
+            leftSum = max(leftSum, sum);
+        }
+        
+        // Find max sum starting from mid+1 and going right
+        int rightSum = INT_MIN;
+        sum = 0;
+        for (int i = mid + 1; i <= right; i++) {
+            sum += nums[i];
+            rightSum = max(rightSum, sum);
+        }
+        
+        // Return sum of both sides
+        return leftSum + rightSum;
+    }
 };
