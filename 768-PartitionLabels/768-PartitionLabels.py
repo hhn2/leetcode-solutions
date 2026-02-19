e372da3757d22208e43cdee9184a58d5641d9277
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2026-02-19 13:34:54 -0500

    Create 768-PartitionLabels

---
+++ 768-PartitionLabels/768-PartitionLabels.py
@@ -0,0 +1,22 @@
+# Last updated: 2/19/2026, 1:34:53 PM
+class Solution(object):
+    def partitionLabels(self, s):
+        """
+        :type s: str
+        :rtype: List[int]
+        """
+        last={}
+        for i in range(len(s)):
+            last[s[i]]=i
+        most=0
+        res=[]
+        c=0
+        for i in range(len(s)):
+            c=c+1
+            if last[s[i]]>most:
+                most=last[s[i]]
+            if last[s[i]]==most and i==most:
+                res.append(c)
+                most=0
+                c=0
+        return res
