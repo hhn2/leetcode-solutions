398d99fb84a4971a2398b5aab79fb89b2a18ea6c
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2026-02-19 13:34:36 -0500

    Create 2042-MaximumProductDifferenceBetweenTwoPairs

---
+++ 2042-MaximumProductDifferenceBetweenTwoPairs/2042-MaximumProductDifferenceBetweenTwoPairs.py
@@ -0,0 +1,15 @@
+# Last updated: 2/19/2026, 1:34:36 PM
+class Solution(object):
+    def maxProductDifference(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        max1 = nums.pop(nums.index(max(nums)))
+        max2 = nums.pop(nums.index(max(nums)))
+
+        min1 = nums.pop(nums.index(min(nums)))
+        min2 = nums.pop(nums.index(min(nums)))
+
+        return ((max1 * max2) - (min1 * min2))
