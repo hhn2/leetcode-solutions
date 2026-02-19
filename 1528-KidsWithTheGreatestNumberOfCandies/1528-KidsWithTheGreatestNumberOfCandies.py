df1b6f09cc57a7d970149a377fe2cce9676c384d
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2026-02-19 13:34:47 -0500

    Create 1528-KidsWithTheGreatestNumberOfCandies

---
+++ 1528-KidsWithTheGreatestNumberOfCandies/1528-KidsWithTheGreatestNumberOfCandies.py
@@ -0,0 +1,12 @@
+# Last updated: 2/19/2026, 1:34:47 PM
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        res = []
+
+        for i in candies:
+            if i + extraCandies >= max(candies):
+                res.append(True)
+            else:
+                res.append(False)
+
+        return res
