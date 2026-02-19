4d6521518eb4bc0d883bfb44020f505acb90ebf1
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2026-02-19 13:34:36 -0500

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 2/19/2026, 1:34:35 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
