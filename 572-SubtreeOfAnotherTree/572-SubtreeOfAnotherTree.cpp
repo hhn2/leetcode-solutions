ecba27363dc5193e6c0506dcaaa48edbd9b87220
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2026-02-19 13:35:01 -0500

    Create 572-SubtreeOfAnotherTree

---
+++ 572-SubtreeOfAnotherTree/572-SubtreeOfAnotherTree.cpp
@@ -0,0 +1,43 @@
+// Last updated: 2/19/2026, 1:35:01 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!root && !subRoot){
+            return true;
+        }
+        if (!root || !subRoot){
+            return false;
+        }
+        if (root->val == subRoot->val){
+            return isSametree(root, subRoot)|| isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        }
+        else{
+            return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        }
+        
+    }
+
+    bool isSametree(TreeNode*root1, TreeNode* root2) {
+        if (!root1 && !root2){
+            return true;
+        }
+        if(!root1 ||!root2){
+            return false;
+        }
+        if (root1->val != root2->val){
+            return false;
+        }
+        return isSametree(root1->left, root2->left) && isSametree(root1->right, root2->right);
+    }
+};
