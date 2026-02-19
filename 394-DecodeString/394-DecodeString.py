04447c4e688efc99d0bf53244414fb172054e9db
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2026-02-19 13:35:02 -0500

    Create 394-DecodeString

---
+++ 394-DecodeString/394-DecodeString.py
@@ -0,0 +1,24 @@
+# Last updated: 2/19/2026, 1:35:02 PM
+class Solution(object):
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] != "]":
+                stack.append(s[i])
+            else:
+                substr = ""
+                while stack[-1] != "[":
+                    substr = stack.pop() + substr
+                stack.pop()
+
+                k = ""
+                while stack and stack[-1].isdigit():
+                    k=stack.pop() + k
+                stack.append(int (k) * substr)
+
+        return "".join(stack)
