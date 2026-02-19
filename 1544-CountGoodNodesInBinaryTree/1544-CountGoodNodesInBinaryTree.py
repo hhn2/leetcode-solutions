a41660088a61c542448be3bfb45819a5b2808aeb
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2026-02-19 13:34:42 -0500

    Create 1544-CountGoodNodesInBinaryTree

---
+++ 1544-CountGoodNodesInBinaryTree/1544-CountGoodNodesInBinaryTree.py
@@ -0,0 +1,19 @@
+# Last updated: 2/19/2026, 1:34:42 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return 1 + self.countGN(root.left, root.val) + self.countGN(root.right, root.val)
+
+    def countGN(self, node: TreeNode, max_val: int) -> int:
+        if not node:
+            return 0
+        if node.val >= max_val:
+            return 1 + self.countGN(node.left, node.val) + self.countGN(node.right, node.val)
+        return self.countGN(node.left, max_val) + self.countGN(node.right, max_val)
