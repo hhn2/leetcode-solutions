af03cbd3ae7af67ac409aa6999e624d187f2b75e
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2026-03-31 17:00:12 -0400

    File updated at 3/31/2026, 5:00:12 PM

--- 135-Candy/135-Candy.cpp
+++ 135-Candy/135-Candy.cpp
@@ -1,4 +1,4 @@
-// Last updated: 3/31/2026, 4:59:47 PM
+// Last updated: 3/31/2026, 5:00:12 PM
 1class Solution {
 2public:
 3    int candy(vector<int>& ratings) {
@@ -8,17 +8,17 @@
 7        }
 8        vector<int> answer;
 9        answer.push_back(1);
-10          cout <<"pushing "<< 1 << endl;
+10        //   cout <<"pushing "<< 1 << endl;
 11        int prev = ratings[0];
 12        for (int i = 1; i < ratings.size(); i++){
 13            if (ratings[i] > prev){
 14                answer.push_back(answer[i-1] + 1);
-15                cout <<"pushing "<< answer[i-1] + 1 << endl;
+15                // cout <<"pushing "<< answer[i-1] + 1 << endl;
 16            }
 17            
 18            else{
 19                answer.push_back(1);
-20                                cout <<"pushing "<< 1 << endl;
+20                                // cout <<"pushing "<< 1 << endl;
 21            }
 22
 23            prev = ratings[i];
@@ -29,11 +29,11 @@
 28        for (int i = ratings.size()-2; i > -1; i--){
 29            cout << i<< endl;
 30            if(ratings[i]>prev){
-31                 cout <<"ratings[i] is "<< ratings[i] << endl;
-32                 cout << "prev is "<< prev << endl;
+31                //  cout <<"ratings[i] is "<< ratings[i] << endl;
+32                //  cout << "prev is "<< prev << endl;
 33                if(answer[i]<=answer[i+1]){
 34                    answer[i] = answer[i+1]+1;
-35                      cout <<"fixed to "<< answer[i] << endl;
+35                    //   cout <<"fixed to "<< answer[i] << endl;
 36                }
 37                
 38            }
