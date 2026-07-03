571e2ce7a5f6d98d5f5bb3e37f5da7974aebd0b6
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2026-07-02 23:17:52 -0400

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.py
@@ -0,0 +1,15 @@
+# Last updated: 7/2/2026, 11:17:52 PM
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+            else:
+                i += 1
+        return len(nums)
