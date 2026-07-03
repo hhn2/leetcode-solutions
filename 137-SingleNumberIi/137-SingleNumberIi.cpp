807d024d508f6a408749a8fef8ea64d085016e4d
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2026-07-02 23:16:55 -0400

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/2/2026, 11:16:55 PM
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        vector<int> bits(32, 0);  
+        
+        for (int num : nums) {
+            for (int i = 0; i < 32; i++) {
+                bits[i] += (num >> i) & 1;
+                bits[i] %= 3;
+            }
+        }
+        
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result |= (bits[i] << i);
+        }
+        
+        return result;
+    }
+};
