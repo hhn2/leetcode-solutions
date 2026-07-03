ab428c5a6c7c022a5d538092ca2653d300207eaa
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2026-07-02 23:16:50 -0400

    Create 149-MaxPointsOnALine

---
+++ 149-MaxPointsOnALine/149-MaxPointsOnALine.py
@@ -0,0 +1,42 @@
+# Last updated: 7/2/2026, 11:16:50 PM
+from math import gcd
+from collections import defaultdict
+
+class Solution:
+    def maxPoints(self, points: list[list[int]]) -> int:
+        n = len(points)
+        if n <= 2:
+            return n
+
+        result = 0
+
+        for i in range(n):
+            slopes = defaultdict(int)
+            curr_max = 0
+
+            for j in range(i + 1, n):
+                dx = points[j][0] - points[i][0]
+                dy = points[j][1] - points[i][1]
+
+            
+                g = gcd(dx, dy)
+                if g != 0:
+                    dx //= g
+                    dy //= g
+
+          
+                if dx == 0:
+                    dy = 1
+                elif dy == 0:
+                    dx = 1
+                elif dx < 0:  
+                    dx = -dx
+                    dy = -dy
+
+                slopes[(dx, dy)] += 1
+                curr_max = max(curr_max, slopes[(dx, dy)])
+
+            result = max(result, curr_max + 1)
+
+        return result
+
