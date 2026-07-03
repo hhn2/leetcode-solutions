2ac4b13bb4117463ae9d8e97f6cb9c1eefffb3fa
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2026-07-02 23:17:55 -0400

    Create 23-MergeKSortedLists

---
+++ 23-MergeKSortedLists/23-MergeKSortedLists.cpp
@@ -0,0 +1,73 @@
+// Last updated: 7/2/2026, 11:17:54 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
+class Solution {
+public:
+
+    ListNode* mergetwo(ListNode* first, ListNode* second){
+        ListNode mynode;
+        ListNode* toreturn = &mynode;
+
+        if ((!first) && (!second)){
+            return nullptr;
+        }
+
+        ListNode* current1 = first;
+        ListNode* current2 = second;
+        ListNode* currentret = toreturn;
+      
+
+
+        while (true){
+            if ((!current1) && (!current2)){
+                break;
+            }
+            else if (!current1){
+                currentret->next = current2;
+                break;
+            }
+            else if (!current2){
+                currentret->next = current1;
+                break;
+            }
+            else if ((current2->val <= current1-> val)){
+                currentret->next = current2;
+                current2 = current2->next;
+                currentret = currentret->next;
+            }
+            else if ((current1->val < current2-> val)){
+                currentret->next = current1;
+                current1 = current1->next;
+                currentret = currentret->next;
+            }
+          
+        }
+        return toreturn->next;
+        }
+
+
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+    if (lists.empty()) return nullptr;
+    
+    int interval = 1;
+    while (interval < lists.size()) {
+        for (int i = 0; i + interval < lists.size(); i += interval * 2) {
+            lists[i] = mergetwo(lists[i], lists[i + interval]);
+        }
+        interval *= 2;
+    }
+    
+    return lists[0];
+}
+};
