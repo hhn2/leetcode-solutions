38a59e7629e5f64cbddf7e2c7206dddb52ce59c9
Hannah Hwang <140469794+hhn2@users.noreply.github.com>
2026-07-02 23:16:36 -0400

    Create 226-InvertBinaryTree

---
+++ 226-InvertBinaryTree/226-InvertBinaryTree.c
@@ -0,0 +1,20 @@
+// Last updated: 7/2/2026, 11:16:35 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* invertTree(struct TreeNode* root) {
+    if (root == NULL){
+        return root;
+    }
+    struct TreeNode* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+    invertTree(root->left);
+    invertTree(root->right);
+    return root;
+}
